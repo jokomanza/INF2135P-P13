ff211c0290ddb5dd278406935dba4fd893894b3a
Joko Supriyanto <jokomanza@gmail.com>
2024-06-19 20:06:07 +0700

    Initial commit

---
+++ src/nexus/joko/inf2135p/p13/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package nexus.joko.inf2135p.p13;
+
+/**
+ *
+ * @author jokosupriyanto
+ */
+public class Main {
+    public static void main(String[] args) {
+        Frame.main(args);
+    }
+}
